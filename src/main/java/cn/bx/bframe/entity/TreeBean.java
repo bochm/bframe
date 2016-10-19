50634d328fe4a2cc6e7bdd229100a179fbe53800
bochm <bochm@163.com>
2016-10-19 23:03:13 +0800

    bcm h

--- src/main/java/cn/bx/bframe/entity/TreeBean.java
+++ src/main/java/cn/bx/bframe/entity/TreeBean.java
@@ -1,32 +1,42 @@
 package cn.bx.bframe.entity;
 import org.apache.commons.lang3.StringUtils;
 
-import cn.bx.bframe.common.util.IdGen;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 public abstract class TreeBean<T extends BaseBean> extends BaseBean{
 	private static final long serialVersionUID = 1L;
 	protected T parent;
 	protected String parentId;//父对象id
-	protected String parentIds;//所有父对象id,以,连接
-	protected String treeSort;//树形排序.所有祖先节点的排序值-id用,相连
+	protected String parentTree;//树形排序.所有祖先节点的sort-id用,相连
 	public TreeBean(){
 		this.sort = 9;
 	}
 	@JsonBackReference
 	public abstract T getParent();
 	public abstract void setParent(T parent);
+	//树形排序使用
+	@JsonProperty("treeSort")
 	public String getTreeSort(){
-		return (StringUtils.isEmpty(this.treeSort) ? "" :  (this.treeSort + ",")) + getSort();
+		return (StringUtils.isNotEmpty(this.parentTree) ? (this.parentTree + ",") :  "") + getSort() + "-" + getId();
 	}
-	public void setTreeSort(String treeSort) {
-		this.treeSort = treeSort;
+	
+	
+	public String getParentTree() {
+		return parentTree;
 	}
+	public void setParentTree(String parentTree) {
+		this.parentTree = parentTree;
+	}
+	//去除treeSort的连接符和sort,用于sql
 	public String getParentIds() {
-		return parentIds;
-	}
-	public void setParentIds(String parentIds) {
-		this.parentIds = parentIds;
+		if(parentTree == null) return null;
+		String[] idSorts = parentTree.split(",");
+		StringBuffer ids = new StringBuffer();
+		for(String idSort : idSorts){
+			if(idSort.indexOf("-") > 0) ids.append("'" + idSort.split("-")[1] + "',");
+			else ids.append("'" + idSort + "',");
+		}
+		return ids.substring(0, ids.length() - 1);
 	}
 	public String getParentId() {
 		if (parent != null && parentId == null){
@@ -40,9 +50,5 @@ public abstract class TreeBean<T extends BaseBean> extends BaseBean{
 		if (parent != null){
 			parent.setId(parentId);
 		}
-	}
-	public String getId(){
-		if(isNewRecord()) setId(IdGen.uuid());
-		return this.id;
 	}
 }
