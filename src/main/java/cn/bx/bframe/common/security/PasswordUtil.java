73e0c28a6543041b6ae1fac76f3a04813dd131a7
bochm <bochm@163.com>
2017-06-14 17:15:55 +0800

    bcm 555555

--- src/main/java/cn/bx/bframe/common/security/PasswordUtil.java
+++ src/main/java/cn/bx/bframe/common/security/PasswordUtil.java
@@ -1,6 +1,8 @@
 package cn.bx.bframe.common.security;
 
 import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
 
 import cn.bx.bframe.common.util.Encodes;
 
@@ -13,7 +15,50 @@ public class PasswordUtil {
 	public static final String HASH_ALGORITHM = "SHA-1";//Hash算法
 	public static final int HASH_INTERATIONS = 1024; //迭代次数
 	public static final int SALT_SIZE = 8;//salt位数,8*2=16
-	
+	/**
+     * MD5加密
+     * @param data 待加密数据
+     * @return byte[] 消息摘要
+     * @throws Exception
+     */
+    public static byte[] encodeMD5(byte[] data){
+        return encodeMD5(data,null,1);
+    }
+
+    /**
+     * MD5加密
+     * @param data 待加密数据 salt 盐
+     * @return byte[] 消息摘要
+     * @throws Exception
+     */
+    public static byte[] encodeMD5(byte[] data,byte[] salt){
+        return encodeMD5(data,salt,1);
+    }
+
+    /**
+     * MD5加密
+     * @param data 待加密数据 salt 盐 iterations 加密次数
+     * @return byte[] 消息摘要
+     * @throws Exception
+     */
+    public static byte[] encodeMD5(byte[] data,byte[] salt,int iterations){
+        try{
+            // 初始化MessageDigest
+            MessageDigest md=MessageDigest.getInstance("MD5");
+            if(salt!=null){
+                md.update(salt);
+            }
+            byte[] result=md.digest(data);
+            for(int i=1;i<iterations;i++){
+                md.reset();
+                result=md.digest(result);
+            }
+            return result;
+        }catch(GeneralSecurityException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 	/**
 	 * 生成随机的16位salt并经过1024次 sha-1 hash迭代的密码
 	 * @param plainPassword 明文密码
@@ -24,7 +69,14 @@ public class PasswordUtil {
 		byte[] hashPassword = Digests.sha1(plainPassword.getBytes(), salt, HASH_INTERATIONS);
 		return Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword);
 	}
-	
+	/**
+	 * 生成随机的16位salt并经过1024次 sha-1 hash迭代的密码
+	 * @param plainPassword 明文密码
+	 * @return 密文密码
+	 */
+	public static String entryptMD5Password(String plainPassword) {
+		return Encodes.encodeHex(encodeMD5(plainPassword.getBytes()));
+	}
 	/**
 	 * 获取密码中的salt
 	 * @param password 包含salt密文密码
@@ -54,6 +106,6 @@ public class PasswordUtil {
 	}
 	
 	public  static void main(String[] args) throws UnsupportedEncodingException{
-		System.out.println(entryptPassword("0000cs55555rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr555ffffffffffffffffffssss222"));
+		System.out.println(entryptMD5Password("bcm123456"));
 	}
 }
